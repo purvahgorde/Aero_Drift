6b82abb706c580bfc0034ee7f1b70dae6666d89a
Shivanandini Saddanapu <nandinisaddanapu@gmail.com>
2026-09-05 15:10:03 +0530

    test: validate AWS resource relationships

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -34,3 +34,57 @@ def test_ec2_instances_have_relationships():
         assert "vpc_id" in instance
         assert "subnet_id" in instance
         assert "security_group_ids" in instance
+
+
+def test_mock_resources_have_required_relationship_fields():
+    mock_file = Path("data/mock_aws_state.json")
+
+    with mock_file.open("r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    for subnet in data["subnets"]:
+        assert "id" in subnet
+        assert "vpc_id" in subnet
+        assert "cidr" in subnet
+
+    for security_group in data["security_groups"]:
+        assert "id" in security_group
+        assert "vpc_id" in security_group
+        assert "ingress_rules" in security_group
+
+
+def test_instance_relationship_ids_match_mock_resources():
+    mock_file = Path("data/mock_aws_state.json")
+
+    with mock_file.open("r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    subnet_ids = {subnet["id"] for subnet in data["subnets"]}
+    security_group_ids = {
+        security_group["id"]
+        for security_group in data["security_groups"]
+    }
+
+    for instance in data["instances"]:
+        assert instance["subnet_id"] in subnet_ids
+
+        for security_group_id in instance["security_group_ids"]:
+            assert security_group_id in security_group_ids
+
+
+def test_mock_vpc_relationships_are_consistent():
+    mock_file = Path("data/mock_aws_state.json")
+
+    with mock_file.open("r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    vpc_ids = {vpc["id"] for vpc in data["vpcs"]}
+
+    for subnet in data["subnets"]:
+        assert subnet["vpc_id"] in vpc_ids
+
+    for security_group in data["security_groups"]:
+        assert security_group["vpc_id"] in vpc_ids
+
+    for instance in data["instances"]:
+        assert instance["vpc_id"] in vpc_ids
